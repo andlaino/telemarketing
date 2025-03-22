8cebe5ae87367f6408c955ef2b7ca067d9b2396b
andlaino <andersonlaino@gmail.com>
2025-03-22 18:40:52 -0300

    Streamlit

--- app_7.py
+++ app_7.py
@@ -25,11 +25,6 @@ def multiselect_filter(relatorio, col, selecionados):
     else:
         return relatorio[relatorio[col].isin(selecionados)].reset_index(drop=True)
 
-# Função para converter DataFrame em CSV
-@st.cache_data
-def convert_df(df):
-    return df.to_csv(index=False).encode('utf-8')
-
 # Função para converter DataFrame em Excel
 @st.cache_data
 def to_excel(df):
@@ -56,7 +51,6 @@ def main():
     st.sidebar.write("## Suba o arquivo")
     data_file_1 = st.sidebar.file_uploader("Bank marketing data", type=['csv', 'xlsx'])
 
-    # Processamento dos dados
     if data_file_1 is not None:
         bank_raw = load_data(data_file_1)
         bank = bank_raw.copy()
@@ -66,7 +60,6 @@ def main():
 
         with st.sidebar.form(key='my_form'):
             graph_type = st.radio('Tipo de gráfico:', ('Barras', 'Pizza'))
-
             max_age = int(bank.age.max())
             min_age = int(bank.age.min())
             idades = st.slider('Idade', min_value=min_age, max_value=max_age, value=(min_age, max_age))
@@ -97,12 +90,10 @@ def main():
                         .pipe(multiselect_filter, 'loan', selections["loan"])
                         .pipe(multiselect_filter, 'contact', selections["contact"])
                         .pipe(multiselect_filter, 'month', selections["month"])
-                        .pipe(multiselect_filter, 'day_of_week', selections["day_of_week"])
-            )
+                        .pipe(multiselect_filter, 'day_of_week', selections["day_of_week"]))
 
             submit_button = st.form_submit_button(label='Aplicar')
 
-        # Exibir tabela filtrada
         st.write('## Após os filtros')
         st.write(bank.head())
 
@@ -112,63 +103,68 @@ def main():
                            file_name='bank_filtered.xlsx')
         st.markdown("---")
 
-        # Criando proporção de aceite
-        fig, ax = plt.subplots(1, 2, figsize=(5, 3))
+        fig, ax = plt.subplots(1, 2, figsize=(10, 4))
 
         bank_raw_target_perc = bank_raw.y.value_counts(normalize=True).to_frame() * 100
-        bank_raw_target_perc.columns = ['y']
+        bank_raw_target_perc.columns = ['Percentual']
         bank_raw_target_perc = bank_raw_target_perc.sort_index()
+        bank_raw_target_perc.index = ['Não', 'Sim']
 
         try:
             bank_target_perc = bank.y.value_counts(normalize=True).to_frame() * 100
-            bank_target_perc.columns = ['y']
+            bank_target_perc.columns = ['Percentual']
             bank_target_perc = bank_target_perc.sort_index()
+            bank_target_perc.index = ['Não', 'Sim']
         except:
             st.error('Erro no filtro')
 
-        # Botões de download dos dados dos gráficos
         col1, col2 = st.columns(2)
-
-        df_xlsx = to_excel(bank_raw_target_perc)
         col1.write('### Proporção original')
         col1.write(bank_raw_target_perc)
-        col1.download_button(label='📥 Download',
-                             data=df_xlsx,
-                             file_name='bank_raw_y.xlsx')
-
-        df_xlsx = to_excel(bank_target_perc)
         col2.write('### Proporção da tabela com filtros')
         col2.write(bank_target_perc)
-        col2.download_button(label='📥 Download',
-                             data=df_xlsx,
-                             file_name='bank_y.xlsx')
 
         st.markdown("---")
         st.write('## Proporção de aceite')
 
-        # PLOTS    
+        colors = ["#ff9999", "#66b3ff"]
+
         if graph_type == 'Barras':
             sns.barplot(x=bank_raw_target_perc.index, 
-                        y='y',
+                        y='Percentual',
                         data=bank_raw_target_perc, 
-                        ax=ax[0])
+                        ax=ax[0], 
+                        palette=colors)
             ax[0].bar_label(ax[0].containers[0])
             ax[0].set_title('Dados brutos', fontweight="bold")
-
+            ax[0].set_xlabel('')
+            
             sns.barplot(x=bank_target_perc.index, 
-                        y='y', 
+                        y='Percentual', 
                         data=bank_target_perc, 
-                        ax=ax[1])
+                        ax=ax[1], 
+                        palette=colors)
             ax[1].bar_label(ax[1].containers[0])
             ax[1].set_title('Dados filtrados', fontweight="bold")
+            ax[1].set_xlabel('')
         else:
-            bank_raw_target_perc.plot(kind='pie', autopct='%.2f%%', ax=ax[0], y='y', labels=bank_raw_target_perc.index)
+            bank_raw_target_perc.plot(kind='pie', 
+                                      autopct='%.2f%%', 
+                                      ax=ax[0], 
+                                      y='Percentual', 
+                                      labels=bank_raw_target_perc.index, 
+                                      colors=colors)
             ax[0].set_title('Dados brutos', fontweight="bold")
-
-            bank_target_perc.plot(kind='pie', autopct='%.2f%%', ax=ax[1], y='y', labels=bank_target_perc.index)
+            
+            bank_target_perc.plot(kind='pie', 
+                                  autopct='%.2f%%', 
+                                  ax=ax[1], 
+                                  y='Percentual', 
+                                  labels=bank_target_perc.index, 
+                                  colors=colors)
             ax[1].set_title('Dados filtrados', fontweight="bold")
 
-        st.pyplot(plt)
+        st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
